a9cf6e0b3518e82b9c734da985bbd63adff461db
ariesevans <ariesevans@gmail.com>
2023-04-19 16:54:26 +0900

    18일 공부

---
+++ choo/sec04/Multi.java
@@ -0,0 +1,15 @@
+package choo.sec04;
+
+public class Multi {
+
+	public static void main(String[] args) {
+		
+		int[][] mathScores=new int[2][3];
+		for(int i=0;i<mathScores.length; i++) {
+			for(int k=0;k<mathScores[i].length; k++ )
+				System.out.println("mathScores["+i+"]["+k+"]:"+mathScores[i][k]);
+		}
+		
+	}
+	
+}
---
+++ choo/sec04/Test12.java
@@ -0,0 +1,10 @@
+package choo.sec04;
+
+public class Test12 {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
